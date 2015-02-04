82f75a67fc11016c5b3feee18b349c1fe729c533
Arven <brian.johnathan.b@gmail.com>
2015-02-04 06:56:37 -0500

    Add time-based plotting.

--- commands.tcl
+++ commands.tcl
@@ -2,4 +2,5 @@ proc setDevice {} {
 }
 
 proc helpAbout {} {
+    tk_messageBox -message "TCL Boiler Monitor v0.1a" -type ok -icon info
 }
--- timespan.tcl
+++ timespan.tcl
@@ -1,17 +1,15 @@
-frame .timespan
-label .timespan.label -text "Time Span"
+labelframe .timespan -text "Time Span"
 radiobutton .timespan.daily -text Daily -var timespan -val d
 radiobutton .timespan.weekly -text Weekly -var timespan -val w
 radiobutton .timespan.biweekly -text Biweekly -var timespan -val bw
 radiobutton .timespan.monthly -text Monthly -var timespan -val m
 radiobutton .timespan.bimonthly -text Bimonthly -var timespan -val bm
 
-pack .timespan.label .timespan -side top -anchor nw -expand yes
 pack .timespan.daily .timespan -side left
 pack .timespan.weekly .timespan -side left
 pack .timespan.biweekly .timespan -side left
 pack .timespan.monthly .timespan -side left
 pack .timespan.bimonthly .timespan -side left
-pack .timespan
+pack .timespan -expand yes
 
 set timespan d
